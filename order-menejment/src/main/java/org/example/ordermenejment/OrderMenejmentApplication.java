30d3417be69f6421d60f54e39ab2d30fd123672e
Bekzod <bekzodnosirov02@gmail.com>
2024-10-01 10:46:55 +0500

    add project

---
+++ order-menejment/src/main/java/org/example/ordermenejment/OrderMenejmentApplication.java
@@ -0,0 +1,13 @@
+package org.example.ordermenejment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderMenejmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMenejmentApplication.class, args);
+    }
+
+}
---
+++ order-menejment/src/test/java/org/example/ordermenejment/OrderMenejmentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ordermenejment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderMenejmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
