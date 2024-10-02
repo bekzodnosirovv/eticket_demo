e9b208e2806c4c8daccdb4bff0b6572147c963a6
Bekzod <bekzodnosirov02@gmail.com>
2024-10-02 16:26:29 +0500

    add aggregate

--- order-menejment/src/main/java/org/example/ordermenejment/command/OrderAggregate.java
+++ order-menejment/src/main/java/org/example/ordermenejment/command/OrderAggregate.java
@@ -1,8 +1,18 @@
 package org.example.ordermenejment.command;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.axonframework.spring.stereotype.Aggregate;
 
+/**
+ * Created by Bekzod Nosirov on 02.10.2024
+   project: e-ticket
+   version: 1.0
+ */
+
+@Slf4j
 @Aggregate
 public class OrderAggregate {
+
+
 }
